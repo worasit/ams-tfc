e33e388088686c31f7e254c0b6bda570436e729d
Worasit D <worasit.dmk501@gmail.com>
2020-09-20 12:39:31 +0700

    Applied google_compute_instance resource.

--- gcp-demo/main.tf
+++ gcp-demo/main.tf
@@ -7,13 +7,27 @@ terraform {
 }
 
 provider "google" {
-  version = "3.5.0"
+  version     = "3.5.0"
   credentials = file("~/nongped-001.json")
-  project = "nongped-001"
-  region = "us-central1"
-  zone = "us-central1-c"
+  project     = "nongped-001"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  machine_type = "f1-micro"
+  name         = "terraform-instance"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+  }
 }
