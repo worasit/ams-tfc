fdc37afa06f0840b94f97b4089258373694fbb47
Worasit D <worasit.dmk501@gmail.com>
2020-09-20 12:02:41 +0700

    Added GCP demo.

---
+++ gcp-demo/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+  credentials = file("~/nongped-001.json")
+  project = "nongped-001"
+  region = "us-central1"
+  zone = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
